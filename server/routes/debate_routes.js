ef501c781db700da2ff192806b27c317fe40a9a4
Abhinav-Saxena-dev <abhinavsaxena148@gmail.com>
2022-11-03 21:08:20 +0530

    changes to server-side

--- server/routes/debate_routes.js
+++ server/routes/debate_routes.js
@@ -4,11 +4,11 @@ const derouter = express.Router();
 
 const Debate = require('./../models/debate')
 
-derouter.get('debate', (req, res) => {
+derouter.get('/debate', (req, res) => {
     res.status(200).json({msg : "The link is wokring"})
 })
 
-derouter.post('debate', (req, res) => {
+derouter.post('/debate', (req, res) => {
     Debate.create(req.body)
         .then(people => res.json(`The person ${JSON.stringify(req.body)} is created and ${people}`))
         .catch(err => res.status(404).json(`The person could not be created with error ${err}`))
