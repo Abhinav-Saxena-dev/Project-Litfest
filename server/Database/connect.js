a2da7d00e15a4a8ba5e31cf26a13533b79de4184
Abhinav-Saxena-dev <abhinavsaxena148@gmail.com>
2022-11-04 03:32:52 +0530

    added new properties to database connect"

--- server/Database/connect.js
+++ server/Database/connect.js
@@ -1,22 +1,23 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const connectDB = async (URL) => {
+  try {
+    await mongoose.connect(
+      URL,
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        autoIndex: true,
+      },
+      () => {
+        console.log("MongoDB connected...");
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
 
-    try{
-        await mongoose.connect(
-            URL,
-            {
-                useNewUrlParser : true,
-                useUnifiedTopology : true,
-            },
-            () => {
-                console.log("MongoDB connected...");
-            }
-        )
-    }catch(err){
-        console.log(err);
-        process.exit(1)
-    }
-}
-
-module.exports = connectDB
+module.exports = connectDB;
