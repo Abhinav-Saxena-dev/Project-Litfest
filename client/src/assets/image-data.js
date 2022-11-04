09dcbb8679a7b0c768929d9164bdcf2c1666cea2
Abhinav-Saxena-dev <abhinavsaxena148@gmail.com>
2022-11-04 11:13:06 +0530

    added images to glimpse

--- client/src/assets/image-data.js
+++ client/src/assets/image-data.js
@@ -1,7 +1,10 @@
 export const images = [
-    './RKJ_1045.JPG',
-    './RKJ_1140.JPG',
-    './RKJ_1145.JPG',
-    './RKJ_1187.JPG'
+"https://i.ibb.co/RChR7w6/RKJ-1187.jpg",
+"https://i.ibb.co/1sH51mg/RKJ-1170.jpg",
+"https://i.ibb.co/p2xzqMF/RKJ-1155.jpg",
+"https://i.ibb.co/vmqtDv7/RKJ-1136.jpg",
+"https://i.ibb.co/tpmPt2m/RKJ-1065.jpg",
+"https://i.ibb.co/68nd6WP/RKJ-1060.jpg",
+"https://i.ibb.co/0MZGjfV/RKJ-1045.jpg",
   ];
   
